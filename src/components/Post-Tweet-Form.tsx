d45bafcf819308564802c6363b62392ecb8205a1
Myomyoring <narcissuscat@naver.com>
2023-12-07 16:01:36 +0900

    refactor: tweet-form separate style components

--- src/components/Post-Tweet-Form.tsx
+++ src/components/Post-Tweet-Form.tsx
@@ -1,59 +1,8 @@
 import { addDoc, collection, updateDoc } from 'firebase/firestore';
 import { useState } from 'react';
-import styled from 'styled-components';
 import { auth, db, storage } from '../firebase';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-
-const Form = styled.form`
-	display: flex;
-	flex-direction: column;
-	gap: 10px;
-`;
-const TextArea = styled.textarea`
-	border: 2px solid white;
-	padding: 20px;
-	border-radius: 20px;
-	font-size: 16px;
-	color: white;
-	background-color: black;
-	width: 100%;
-	resize: none;
-	font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
-		'Helvetica Neue', sans-serif;
-	&::placeholder {
-		font-size: 16px;
-	}
-	&:focus {
-		outline: none;
-		border-color: #8cc152;
-	}
-`;
-const AttachFileButton = styled.label`
-	padding: 10px 0px;
-	color: #8cc152;
-	text-align: center;
-	border-radius: 20px;
-	border: 1px solid #8cc152;
-	font-size: 14px;
-	font-weight: 600;
-	cursor: pointer;
-`;
-const AttachFileInput = styled.input`
-	display: none;
-`;
-const SubmitBtn = styled.input`
-	background-color: #8cc152;
-	color: white;
-	border: none;
-	padding: 10px 0px;
-	border-radius: 20px;
-	font-size: 16px;
-	cursor: pointer;
-	&:hover,
-	&:active {
-		opacity: 0.9;
-	}
-`;
+import { AttachFileButton, AttachFileInput, Form, SubmitBtn, TextArea } from './Tweet-Form-Components';
 
 export default function PostTweetForm() {
 	const [isLoading, setLoading] = useState(false);
@@ -64,7 +13,6 @@ export default function PostTweetForm() {
 	};
 	const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { files } = e.target;
-		console.log(files);
 		const maxSize = 1 * 1024 * 1024; // 1MB
 		if (files && files.length === 1) {
 			if (files[0].size > maxSize) {
