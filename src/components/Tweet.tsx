60012f5c7b495ca04cfa7c27300053e9195faf70
Myomyoring <narcissuscat@naver.com>
2023-12-07 16:01:53 +0900

    feat: tweet update

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import { ITweet } from './TimeLine';
 import { auth, db, storage } from '../firebase';
-import { deleteDoc, doc } from 'firebase/firestore';
-import { deleteObject, ref } from 'firebase/storage';
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { AttachFileButton, AttachFileInput, Form, SubmitBtn, TextArea } from './Tweet-Form-Components';
+import { useState } from 'react';
 
 const Wrapper = styled.div`
 	display: grid;
@@ -15,6 +17,9 @@ const Wrapper = styled.div`
 const Column = styled.div`
 	&:last-child {
 		place-self: end;
+	}
+	.btn {
+		margin-right: 4px;
 	}
 `;
 
@@ -46,6 +51,18 @@ const DeleteButton = styled.button`
 	cursor: pointer;
 `;
 
+const UpdateButton = styled.button`
+	background-color: transparent;
+	color: #8cc152;
+	font-weight: 600;
+	border: 1px solid #8cc152;
+	font-size: 12px;
+	padding: 5px 10px;
+	text-transform: uppercase;
+	border-radius: 5px;
+	cursor: pointer;
+`;
+
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
 	const user = auth.currentUser;
 	const onDelete = async () => {
@@ -63,14 +80,88 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
 			//
 		}
 	};
+	// update
+	const [updateMode, setUpdateMode] = useState(false);
+	const [isLoading, setLoading] = useState(false);
+	const [updateTweet, setUpdateTweet] = useState(tweet);
+	const [file, setFile] = useState<File | null>(null);
+	const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setUpdateTweet(e.target.value);
+	};
+	const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { files } = e.target;
+		const maxSize = 1 * 1024 * 1024; // 1MB
+		if (files && files.length === 1) {
+			if (files[0].size > maxSize) {
+				alert('이미지는 1MB 이하만 추가 가능합니다.');
+				return;
+			}
+			setFile(files[0]);
+		}
+	};
+
+	const onUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const user = auth.currentUser;
+		if (!user || isLoading || tweet === '' || tweet.length > 180) return;
+		try {
+			setLoading(true);
+			const locationDoc = doc(db, 'tweets', id);
+			await updateDoc(locationDoc, {
+				tweet: updateTweet,
+			});
+			if (file) {
+				const locationRef = ref(storage, `tweets/${user.uid}/${id}`);
+				const result = await uploadBytes(locationRef, file);
+				const url = await getDownloadURL(result.ref);
+				await updateDoc(locationDoc, {
+					photo: url,
+				});
+			}
+			setUpdateTweet('');
+			setFile(null);
+			setUpdateMode(false);
+		} catch (err) {
+			console.log(err);
+		} finally {
+			setLoading(false);
+		}
+	};
 	return (
-		<Wrapper>
-			<Column>
-				<Username>{username}</Username>
-				<Payload>{tweet}</Payload>
-				{user?.uid === userId ? <DeleteButton onClick={onDelete}>삭제하기</DeleteButton> : null}
-			</Column>
-			<Column>{photo ? <Photo src={photo} /> : null}</Column>
-		</Wrapper>
+		<>
+			{!updateMode ? (
+				<Wrapper>
+					<Column>
+						<Username>{username}</Username>
+						<Payload>{tweet}</Payload>
+						{user?.uid === userId ? (
+							<UpdateButton className="btn" onClick={() => setUpdateMode(true)}>
+								수정하기
+							</UpdateButton>
+						) : null}
+						{user?.uid === userId ? (
+							<DeleteButton className="btn" onClick={onDelete}>
+								삭제하기
+							</DeleteButton>
+						) : null}
+					</Column>
+					<Column>{photo ? <Photo src={photo} /> : null}</Column>
+				</Wrapper>
+			) : (
+				<Form onSubmit={onUpdate}>
+					<TextArea
+						required
+						rows={5}
+						maxLength={180}
+						onChange={onChange}
+						value={updateTweet}
+						placeholder="무슨 일이 일어나고 있나요?"
+					/>
+					<AttachFileButton htmlFor="updateFile">{file ? '이미지 교체 완료 ✅' : '이미지 교체'}</AttachFileButton>
+					<AttachFileInput onChange={onFileChange} type="file" id="updateFile" accept="image/*" />
+					<SubmitBtn type="submit" value={isLoading ? '게시 중...' : '수정하기'} />
+				</Form>
+			)}
+		</>
 	);
 }
